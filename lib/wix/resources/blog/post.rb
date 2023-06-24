b2b926aa62b9bef0b70e4efad1a3175583c5eeca
Marshall Shen <shen.marshall@gmail.com>
2023-06-24 13:31:04 -0500

    add offsets

--- lib/wix/resources/blog/post.rb
+++ lib/wix/resources/blog/post.rb
@@ -1,22 +1,41 @@
 module Wix
   module Blog
     class Post < ::Wix::APIResource
-      def self.list(opts: {})
-        request_wix_object(
-          method: :get,
-          path: "blog/v3/posts",
-        )
-      end
-
       SUPPORTED_FIELDSETS = %w(
         UNKNOWN COUNTERS URL CONTENT_TEXT METRICS SEO CONTENT INTERNAL_ID CONTACT_ID RICH_CONTENT
       ).freeze
+
+      # @opts params
+      # paging_limit: number of items to load
+      # paging_offset: number of items to skip in the current order
+      # fieldsets: supported values - UNKNOWN COUNTERS URL CONTENT_TEXT METRICS SEO CONTENT INTERNAL_ID CONTACT_ID RICH_CONTENT
+      def self.list(opts: {})
+        query_string = String.new
+        if opts.has_key?(:paging_limit)
+          query_string << "paging.limit=#{opts.fetch(:paging_limit)}&"
+        end
+
+        if opts.has_key?(:paging_offset)
+          query_string << "paging.offset=#{opts.fetch(:paging_offset)}&"
+        end
+
+        if opts.has_key?(:fieldsets)
+          query_string << SUPPORTED_FIELDSETS.find_all{|field| opts.fetch(:fieldsets).include?(field) }
+            .map{|field| "fieldsets=#{field}"}
+            .join("&") 
+        end
+
+        request_wix_object(
+          method: :get,
+          path: "blog/v3/posts",
+          query: query_string,
+        )
+      end
+
       def self.get(post_id, fieldsets=[])
         query_string = SUPPORTED_FIELDSETS.find_all{|field| fieldsets.include?(field) }
           .map{|field| "fieldsets=#{field}"}
           .join("&")
-
-        puts "query_string: #{query_string}"
 
         request_wix_object(
           method: :get,
--- lib/wix/resources/member.rb
+++ lib/wix/resources/member.rb
@@ -1,16 +1,43 @@
 module Wix
   class Member < ::Wix::APIResource
+    SUPPORTED_FIELDSETS = %w(PUBLIC FULL EXTENDED).freeze
+
+    # @opts params
+    # paging_limit: number of items to load
+    # paging_offset: number of items to skip in the current order
+    # fieldsets: supported values - PUBLIC, FULL, EXTENDED, default to PUBLIC
     def self.list(opts: {})
+      query_string = String.new
+      if opts.has_key?(:paging_limit)
+        query_string << "paging.limit=#{opts.fetch(:paging_limit)}&"
+      end
+
+      if opts.has_key?(:paging_offset)
+        query_string << "paging.offset=#{opts.fetch(:paging_offset)}&"
+      end
+
+      if opts.has_key?(:fieldsets)
+        query_string << SUPPORTED_FIELDSETS.find_all{|field| opts.fetch(:fieldsets).include?(field) }
+          .map{|field| "fieldsets=#{field}"}
+          .join("&")
+      end
+
       request_wix_object(
         method: :get,
         path: "members/v1/members",
+        query: query_string,
       )
     end
 
-    def self.get(member_id)
+    def self.get(member_id, fieldsets=[])
+      query_string = SUPPORTED_FIELDSETS.find_all{|field| fieldsets.include?(field) }
+      .map{|field| "fieldsets=#{field}"}
+      .join("&")
+
       request_wix_object(
         method: :get,
-        path: "members/v1/members/#{member_id}"
+        path: "members/v1/members/#{member_id}",
+        query: query_string,
       )
     end
   end
